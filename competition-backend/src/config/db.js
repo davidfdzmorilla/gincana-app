7d77d9ba9a3f0bc6fd44a952ac2faa95273d9a62
David Fernandez <david.fernandez@gestiondeformacion.es>
2024-09-12 10:33:53 +0200

    Change name dir backend

---
+++ competition-backend/src/config/db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+// Cargar variables de entorno
+dotenv.config();
+
+// Crear la conexión a la base de datos
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+// Probar la conexión
+db.connect((err) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+    return;
+  }
+  console.log('Conectado a la base de datos MySQL');
+});
+
+module.exports = db;
