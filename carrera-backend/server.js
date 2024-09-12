7e16814c0460b066fcbd1b1ade094189f26bab66
davidFdzMorilla <92088917+davidfdzmorilla@users.noreply.github.com>
2024-09-12 10:35:06 +0200

    Delete carrera-backend directory

--- carrera-backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/app');
-const db = require('./src/config/db'); // Cargar la conexión a la base de datos
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
